a96042372869126035a1e60cad0cb152def7c7f0
Torsten Scholz <doaschdn@googlemail.com>
2016-05-27 17:43:03 +0200

    MB-5 Add base fragments

---
+++ app/src/main/java/de/doaschdn/muensterbus/tabs/FavoritesFragment.java
@@ -0,0 +1,19 @@
+package de.doaschdn.muensterbus.tabs;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import de.doaschdn.muensterbus.R;
+
+public class FavoritesFragment extends Fragment {
+    public FavoritesFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_favorites, container, false);
+    }
+}
