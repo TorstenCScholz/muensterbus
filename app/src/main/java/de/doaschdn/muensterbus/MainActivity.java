b2e69163b7b835542104ada18275292148916715
Torsten Scholz <doaschdn@googlemail.com>
2016-05-28 18:31:37 +0200

    MB-5 Move bus stop listings to fragment

--- app/src/main/java/de/doaschdn/muensterbus/MainActivity.java
+++ app/src/main/java/de/doaschdn/muensterbus/MainActivity.java
@@ -1,156 +1,92 @@
 package de.doaschdn.muensterbus;
 
-import android.app.AlertDialog;
 import android.app.SearchManager;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.CompoundButton;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.RelativeLayout;
-import android.widget.Spinner;
-import android.widget.TextView;
 
 import com.google.gson.Gson;
 
-import java.text.MessageFormat;
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
 import java.util.List;
 
-import butterknife.Bind;
-import butterknife.ButterKnife;
-import de.doaschdn.muensterbus.thirdparty.DividerItemDecoration;
+import de.doaschdn.muensterbus.tabs.BusStopListingFragment;
+import de.doaschdn.muensterbus.tabs.TabPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    @Bind(R.id.swipe_refresh)
-    SwipeRefreshLayout _swipeRefreshLayout;
-    @Bind(R.id.departure_list)
-    RecyclerView _departureView;
-    @Bind(R.id.rdgSelectedDestination)
-    RadioGroup _rdgSelectedDestination;
-    @Bind(R.id.station_spinner)
-    Spinner _spStations;
-    @Bind(R.id.loading_panel)
-    RelativeLayout _loadingPanel;
-
-    private BusStopGroup _selectedBusStopGroup;
-
-    class QuerySearchForSpecificTerm extends AsyncTask<String, Void, List<BusStopGroup>> {
-
-        private String _busStopName;
-
-        @Override
-        protected List<BusStopGroup> doInBackground(String... params) {
-            _busStopName = params[0];
-            return SearchContentProvider.getBusStopGroupsFor(_busStopName);
-        }
-
-        @Override
-        protected void onPostExecute(List<BusStopGroup> busStopGroups) {
-            if (busStopGroups.size() != 1) {
-                new AlertDialog.Builder(MainActivity.this)
-                        .setTitle(R.string.busstop_not_found)
-                        .setMessage(MessageFormat.format(getText(R.string.busstop_not_found_desc).toString(), _busStopName))
-                        .setCancelable(true)
-                        .setPositiveButton("Ok", null)
-                        .show();
-            } else {
-                setBusStopGroup(busStopGroups.get(0));
-            }
-        }
-    }
-
-    class BusStopRequest extends AsyncTask<BusStop, Void, List<Departure>> {
-
-        private final SWMApiEndpointInterface client = SWMClient.createService(SWMApiEndpointInterface.class);
-
-        @Override
-        protected List<Departure> doInBackground(BusStop... params) {
-            return SWMParser.parseBusStopRequests(
-                    client.getDeparturesForBusStop(
-                            params[0].getId(),
-                            System.currentTimeMillis() / 1000
-                    )
-            );
-        }
-
-        @Override
-        protected void onPostExecute(List<Departure> departureList) {
-            runOnUiThread(new Runnable() {
-                public void run() {
-                    _swipeRefreshLayout.setRefreshing(false);
-                    displayLoadingPanel(false);
-                }
-            });
-
-            DepartureAdapter adapter = new DepartureAdapter(getApplicationContext(), departureList);
-            _departureView.setAdapter(adapter);
-        }
-    }
+    List<WeakReference<Fragment>> _fragments = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
-        ButterKnife.bind(this);
+        //ButterKnife.bind(this);
 
-        _swipeRefreshLayout.setColorSchemeResources(R.color.primary_dark, R.color.primary, R.color.primary_light);
-        _swipeRefreshLayout.setOnRefreshListener(getOnRefreshListener());
-
-        initDepartureView();
+        initTabLayout();
 
         handleIntent(getIntent());
 
         setDefaultKeyMode(DEFAULT_KEYS_SEARCH_LOCAL);
     }
 
-    @NonNull
-    private SwipeRefreshLayout.OnRefreshListener getOnRefreshListener() {
-        return new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                if (_selectedBusStopGroup == null) { // Nothing to load
-                    _swipeRefreshLayout.setRefreshing(false);
-                    return;
-                }
+    private void initTabLayout() {
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText(getText(R.string.bus_stop_listing)));
+        tabLayout.addTab(tabLayout.newTab().setText(getText(R.string.favorites)));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
-                BusStop busStop;
-
-                if (_selectedBusStopGroup.containsStation()) {
-                    busStop = ((BusStopSpinnerWrapper) _spStations.getSelectedItem()).getBusStop();
-                } else {
-                    int radioButtonID = _rdgSelectedDestination.getCheckedRadioButtonId();
-                    RadioButton selectedRadioButton = (RadioButton) _rdgSelectedDestination.findViewById(radioButtonID);
-                    busStop = (BusStop) selectedRadioButton.getTag();
-                }
-
-                updateDepartures(busStop);
-            }
-        };
+        initTabAdapter(tabLayout);
     }
 
-    private void initDepartureView() {
-        _departureView.setHasFixedSize(true);
-        LinearLayoutManager llm = new LinearLayoutManager(this);
-        llm.setOrientation(LinearLayoutManager.VERTICAL);
-        _departureView.setLayoutManager(llm);
-        DividerItemDecoration divider = new DividerItemDecoration(this, null);
-        _departureView.addItemDecoration(divider);
+    @Override
+    public void onAttachFragment (Fragment fragment) {
+        _fragments.add(new WeakReference(fragment));
+    }
 
-        displayLoadingPanel(false);
+    public List<Fragment> getActiveFragments() {
+        ArrayList<Fragment> ret = new ArrayList<>();
+
+        for(WeakReference<Fragment> ref : _fragments) {
+            Fragment f = ref.get();
+            if(f != null) {
+                if(f.isVisible()) {
+                    ret.add(f);
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    private void initTabAdapter(TabLayout tabLayout) {
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.tab_pager);
+        final PagerAdapter adapter = new TabPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
     }
 
     @Override
@@ -169,99 +105,13 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d(TAG, "Handling search for: " + query);
         Log.d(TAG, "Data: " + (data != null ? data.toString() : "<null>"));
+        BusStopListingFragment fragment = (BusStopListingFragment)getActiveFragments().get(0);
 
         if (query != null) {
-            new QuerySearchForSpecificTerm().execute(query);
+            fragment.executeQuery(query);
         } else if (data != null) {
-            setBusStopGroup(new Gson().fromJson(data.toString(), BusStopGroup.class));
+            fragment.setBusStopGroup(new Gson().fromJson(data.toString(), BusStopGroup.class));
         }
-    }
-
-    private void setBusStopGroup(BusStopGroup busStopGroup) {
-        TextView tvBusStopName = (TextView) findViewById(R.id.busstop_name);
-        tvBusStopName.setText(busStopGroup.getName());
-
-        _rdgSelectedDestination.removeAllViews();
-        _selectedBusStopGroup = busStopGroup;
-
-        if (busStopGroup.containsStation()) {
-            initBusStopStations(busStopGroup);
-        } else {
-            initBusStopGroup(busStopGroup);
-        }
-    }
-
-    private void initBusStopGroup(BusStopGroup busStopGroup) {
-        enableStationSelection(false);
-
-        for (final BusStop busStop : busStopGroup.getBusStops()) {
-            RadioButton rdBtnBusStop = new RadioButton(this);
-            rdBtnBusStop.setText(busStop.getDirection());
-            rdBtnBusStop.setTag(busStop);
-            rdBtnBusStop.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                BusStop _busStop = busStop;
-
-                @Override
-                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    if (isChecked) {
-                        updateDepartures(_busStop);
-                    }
-                }
-            });
-            _rdgSelectedDestination.addView(rdBtnBusStop);
-        }
-
-        preselectDirection();
-    }
-
-    private void enableStationSelection(boolean enabled) {
-        if (enabled) {
-            _rdgSelectedDestination.setVisibility(View.GONE);
-            _spStations.setVisibility(View.VISIBLE);
-        }
-        else {
-            _spStations.setVisibility(View.GONE);
-            _rdgSelectedDestination.setVisibility(View.VISIBLE);
-        }
-    }
-
-    private void initBusStopStations(BusStopGroup busStopGroup) {
-        enableStationSelection(true);
-
-        ArrayAdapter<BusStopSpinnerWrapper> adapter = new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_spinner_dropdown_item,
-                BusStopSpinnerWrapper.fromBusStopList(busStopGroup.getBusStops())
-        );
-        _spStations.setAdapter(adapter);
-        _spStations.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                BusStopSpinnerWrapper busStopWrapper = (BusStopSpinnerWrapper) parent.getAdapter().getItem(position);
-                updateDepartures(busStopWrapper.getBusStop());
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
-    }
-
-    private void preselectDirection() {
-        RadioButton selectedDirection = (RadioButton) _rdgSelectedDestination.getChildAt(0);
-        selectedDirection.setChecked(true);
-    }
-
-    private void updateDepartures(BusStop busStop) {
-        Log.d(TAG, "New departure: " + busStop.toString());
-
-        displayLoadingPanel(true);
-
-        new BusStopRequest().execute(busStop);
-    }
-
-    private void displayLoadingPanel(boolean show) {
-        _loadingPanel.setVisibility(show ? View.VISIBLE : View.GONE);
     }
 
     @Override
--- app/src/main/java/de/doaschdn/muensterbus/tabs/BusStopListingFragment.java
+++ app/src/main/java/de/doaschdn/muensterbus/tabs/BusStopListingFragment.java
@@ -1,19 +1,255 @@
 package de.doaschdn.muensterbus.tabs;
 
+import android.app.AlertDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import java.text.MessageFormat;
+import java.util.List;
+
+import de.doaschdn.muensterbus.BusStop;
+import de.doaschdn.muensterbus.BusStopGroup;
+import de.doaschdn.muensterbus.BusStopSpinnerWrapper;
+import de.doaschdn.muensterbus.Departure;
+import de.doaschdn.muensterbus.DepartureAdapter;
 import de.doaschdn.muensterbus.R;
+import de.doaschdn.muensterbus.SWMApiEndpointInterface;
+import de.doaschdn.muensterbus.SWMClient;
+import de.doaschdn.muensterbus.SWMParser;
+import de.doaschdn.muensterbus.SearchContentProvider;
+import de.doaschdn.muensterbus.thirdparty.DividerItemDecoration;
 
 public class BusStopListingFragment extends Fragment {
+    private final static String TAG = "BusStopListingFragment";
+
+    View _view;
+    SwipeRefreshLayout _swipeRefreshLayout;
+    RecyclerView _departureView;
+    RadioGroup _rdgSelectedDestination;
+    Spinner _spStations;
+    RelativeLayout _loadingPanel;
+
+    private BusStopGroup _selectedBusStopGroup;
+
     public BusStopListingFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_bus_stop_listing, container, false);
+        _view = inflater.inflate(R.layout.fragment_bus_stop_listing, container, false);
+
+        initBindings();
+        initDepartureView();
+
+        _swipeRefreshLayout.setColorSchemeResources(R.color.primary_dark, R.color.primary, R.color.primary_light);
+        _swipeRefreshLayout.setOnRefreshListener(getOnRefreshListener());
+
+        return _view;
+    }
+
+    private void initBindings() {
+        _swipeRefreshLayout = (SwipeRefreshLayout)_view.findViewById(R.id.swipe_refresh);
+        _departureView = (RecyclerView)_view.findViewById(R.id.departure_list);
+        _rdgSelectedDestination = (RadioGroup)_view.findViewById(R.id.rdgSelectedDestination);
+        _spStations = (Spinner)_view.findViewById(R.id.station_spinner);
+        _loadingPanel = (RelativeLayout)_view.findViewById(R.id.loading_panel);
+    }
+
+    private void initDepartureView() {
+        _departureView.setHasFixedSize(true);
+        LinearLayoutManager llm = new LinearLayoutManager(getContext());
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        _departureView.setLayoutManager(llm);
+        DividerItemDecoration divider = new DividerItemDecoration(getContext(), null);
+        _departureView.addItemDecoration(divider);
+
+        displayLoadingPanel(false);
+    }
+
+    @NonNull
+    private SwipeRefreshLayout.OnRefreshListener getOnRefreshListener() {
+        return new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                if (_selectedBusStopGroup == null) { // Nothing to load
+                    _swipeRefreshLayout.setRefreshing(false);
+                    return;
+                }
+
+                BusStop busStop;
+
+                if (_selectedBusStopGroup.containsStation()) {
+                    busStop = ((BusStopSpinnerWrapper) _spStations.getSelectedItem()).getBusStop();
+                } else {
+                    int radioButtonID = _rdgSelectedDestination.getCheckedRadioButtonId();
+                    RadioButton selectedRadioButton = (RadioButton) _rdgSelectedDestination.findViewById(radioButtonID);
+                    busStop = (BusStop) selectedRadioButton.getTag();
+                }
+
+                updateDepartures(busStop);
+            }
+        };
+    }
+
+    private void updateDepartures(BusStop busStop) {
+        Log.d(TAG, "New departure: " + busStop.toString());
+
+        displayLoadingPanel(true);
+
+        new BusStopRequest().execute(busStop);
+    }
+
+    public void setBusStopGroup(BusStopGroup busStopGroup) {
+        TextView tvBusStopName = (TextView) _view.findViewById(R.id.busstop_name);
+        tvBusStopName.setText(busStopGroup.getName());
+
+        _rdgSelectedDestination.removeAllViews();
+        _selectedBusStopGroup = busStopGroup;
+
+        if (busStopGroup.containsStation()) {
+            initBusStopStations(busStopGroup);
+        } else {
+            initBusStopGroup(busStopGroup);
+        }
+    }
+
+    private void initBusStopGroup(BusStopGroup busStopGroup) {
+        enableStationSelection(false);
+
+        for (final BusStop busStop : busStopGroup.getBusStops()) {
+            RadioButton rdBtnBusStop = new RadioButton(getContext());
+            rdBtnBusStop.setText(busStop.getDirection());
+            rdBtnBusStop.setTag(busStop);
+            rdBtnBusStop.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                BusStop _busStop = busStop;
+
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if (isChecked) {
+                        updateDepartures(_busStop);
+                    }
+                }
+            });
+            _rdgSelectedDestination.addView(rdBtnBusStop);
+        }
+
+        preselectDirection();
+    }
+
+    private void enableStationSelection(boolean enabled) {
+        if (enabled) {
+            _rdgSelectedDestination.setVisibility(View.GONE);
+            _spStations.setVisibility(View.VISIBLE);
+        }
+        else {
+            _spStations.setVisibility(View.GONE);
+            _rdgSelectedDestination.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void initBusStopStations(BusStopGroup busStopGroup) {
+        enableStationSelection(true);
+
+        ArrayAdapter<BusStopSpinnerWrapper> adapter = new ArrayAdapter<>(
+                getContext(),
+                android.R.layout.simple_spinner_dropdown_item,
+                BusStopSpinnerWrapper.fromBusStopList(busStopGroup.getBusStops())
+        );
+        _spStations.setAdapter(adapter);
+        _spStations.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                BusStopSpinnerWrapper busStopWrapper = (BusStopSpinnerWrapper) parent.getAdapter().getItem(position);
+                updateDepartures(busStopWrapper.getBusStop());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+
+    private void preselectDirection() {
+        RadioButton selectedDirection = (RadioButton) _rdgSelectedDestination.getChildAt(0);
+        selectedDirection.setChecked(true);
+    }
+
+    private void displayLoadingPanel(boolean show) {
+        _loadingPanel.setVisibility(show ? View.VISIBLE : View.GONE);
+    }
+
+    public void executeQuery(String query) {
+        new QuerySearchForSpecificTerm().execute(query);
+    }
+
+
+    class QuerySearchForSpecificTerm extends AsyncTask<String, Void, List<BusStopGroup>> {
+
+        private String _busStopName;
+
+        @Override
+        protected List<BusStopGroup> doInBackground(String... params) {
+            _busStopName = params[0];
+            return SearchContentProvider.getBusStopGroupsFor(_busStopName);
+        }
+
+        @Override
+        protected void onPostExecute(List<BusStopGroup> busStopGroups) {
+            if (busStopGroups.size() != 1) {
+                new AlertDialog.Builder(getContext())
+                        .setTitle(R.string.busstop_not_found)
+                        .setMessage(MessageFormat.format(getText(R.string.busstop_not_found_desc).toString(), _busStopName))
+                        .setCancelable(true)
+                        .setPositiveButton("Ok", null)
+                        .show();
+            } else {
+                setBusStopGroup(busStopGroups.get(0));
+            }
+        }
+    }
+
+    class BusStopRequest extends AsyncTask<BusStop, Void, List<Departure>> {
+
+        private final SWMApiEndpointInterface client = SWMClient.createService(SWMApiEndpointInterface.class);
+
+        @Override
+        protected List<Departure> doInBackground(BusStop... params) {
+            return SWMParser.parseBusStopRequests(
+                    client.getDeparturesForBusStop(
+                            params[0].getId(),
+                            System.currentTimeMillis() / 1000
+                    )
+            );
+        }
+
+        @Override
+        protected void onPostExecute(List<Departure> departureList) {
+//            runOnUiThread(new Runnable() {
+//                public void run() {
+//                    _swipeRefreshLayout.setRefreshing(false);
+//                    displayLoadingPanel(false);
+//                }
+//            });
+
+            DepartureAdapter adapter = new DepartureAdapter(getContext(), departureList);
+            _departureView.setAdapter(adapter);
+        }
     }
 }
