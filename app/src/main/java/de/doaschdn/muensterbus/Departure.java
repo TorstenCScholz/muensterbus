4b63a44b25c59498905ecd616de9ce74f0a4a08c
Torsten Scholz <doaschdn@googlemail.com>
2017-03-26 17:35:45 +0200

    NTR Add optional return for transformation of departure times for better reusability

--- app/src/main/java/de/doaschdn/muensterbus/Departure.java
+++ app/src/main/java/de/doaschdn/muensterbus/Departure.java
@@ -7,6 +7,8 @@ import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import java8.util.Optional;
+
 public class Departure {
     private static final long ONE_SECOND_IN_MILLIS = 1000;
     private static final long ONE_MINUTE_IN_MILLIS = 60 * ONE_SECOND_IN_MILLIS;
@@ -44,52 +46,72 @@ public class Departure {
                 break;
             case DEPARTURE_IN:
                 _departureIn = _departureValue;
-                _departureAt = transformDepartureInToDepartureAt(_departureValue);
+                _departureAt = getTransformedDepartureAtText(_departureValue);
                 break;
             case DEPARTURE_AT:
-                _departureIn = transformDepartureAtToDepartureIn(_departureValue);
+                _departureIn = getTransformedDepartureInText(_departureValue);
                 _departureAt = _departureValue;
                 break;
         }
     }
 
-    private String transformDepartureAtToDepartureIn(String departureAt) {
-        Matcher m = PATTERN_DEPARTURE_AT.matcher(departureAt);
+    private String getTransformedDepartureAtText(String departureValue) {
+        Optional<String> departureAtOptional = transformDepartureInToDepartureAt(departureValue);
 
-        if (m.matches()) {
-            int hours = Integer.parseInt(m.group(1));
-            int minutes = Integer.parseInt(m.group(2));
-
-            long arrivalInMillis = minutes * ONE_MINUTE_IN_MILLIS + hours * ONE_HOUR_IN_MILLIS;
-
-            Date now = new Date();
-
-            // Check, if arrival is on a new day (i.e. now=23:55, arrival=00:42)
-            if (now.getHours() > hours) {
-                arrivalInMillis += ONE_DAY_IN_MILLIS;
-            }
-
-            long nowInMillis = now.getHours() * ONE_HOUR_IN_MILLIS + now.getMinutes() * ONE_MINUTE_IN_MILLIS;
-
-            return ((arrivalInMillis - nowInMillis) / ONE_MINUTE_IN_MILLIS) + "min";
+        if (departureAtOptional.isPresent()) {
+            return departureAtOptional.get();
         }
 
         return "";
     }
 
-    private String transformDepartureInToDepartureAt(String departureIn) {
-        Matcher m = PATTERN_DEPARTURE_IN.matcher(departureIn);
+    private String getTransformedDepartureInText(String departureValue) {
+        Optional<String> departureInOptional = transformDepartureAtToDepartureIn(departureValue);
 
-        if (m.matches()) {
-            int minUntilArrival = Integer.parseInt(m.group(1));
-            Calendar cal = Calendar.getInstance();
-            cal.setTime(new Date());
-            cal.add(Calendar.MINUTE, minUntilArrival);
-
-            return new SimpleDateFormat("HH:mm", Locale.GERMANY).format(cal.getTime());
+        if (departureInOptional.isPresent()) {
+            return departureInOptional.get();
         }
 
         return "";
+    }
+
+    private Optional<String> transformDepartureAtToDepartureIn(String departureAt) {
+        Matcher m = PATTERN_DEPARTURE_AT.matcher(departureAt);
+
+        if (!m.matches()) {
+            return Optional.empty();
+        }
+
+        int hours = Integer.parseInt(m.group(1));
+        int minutes = Integer.parseInt(m.group(2));
+
+        long arrivalInMillis = minutes * ONE_MINUTE_IN_MILLIS + hours * ONE_HOUR_IN_MILLIS;
+
+        Date now = new Date();
+
+        // Check, if arrival is on a new day (i.e. now=23:55, arrival=00:42)
+        if (now.getHours() > hours) {
+            arrivalInMillis += ONE_DAY_IN_MILLIS;
+        }
+
+        long nowInMillis = now.getHours() * ONE_HOUR_IN_MILLIS + now.getMinutes() * ONE_MINUTE_IN_MILLIS;
+
+        return Optional.of(((arrivalInMillis - nowInMillis) / ONE_MINUTE_IN_MILLIS) + "min");
+    }
+
+    private Optional<String> transformDepartureInToDepartureAt(String departureIn) {
+        Matcher m = PATTERN_DEPARTURE_IN.matcher(departureIn);
+
+        if (!m.matches()) {
+            return Optional.empty();
+        }
+
+        int minUntilArrival = Integer.parseInt(m.group(1));
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+        cal.add(Calendar.MINUTE, minUntilArrival);
+
+        return Optional.of(new SimpleDateFormat("HH:mm", Locale.GERMANY).format(cal.getTime()));
     }
 
     public String getBusLine() {
